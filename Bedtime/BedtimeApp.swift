6cb1de6b04ed5f71d6c65548cbb284a7efab58c4
Om Chachad <omchachad12@gmail.com>
2025-06-03 17:27:41 +0530

    Initial Commit

---
+++ Bedtime/BedtimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BedtimeApp.swift
+//  Bedtime
+//
+//  Created by Om Chachad on 6/3/25.
+//
+
+import SwiftUI
+
+@main
+struct BedtimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
